b6579a995d2162a48ffaa6137f2e0f47341a8115
Linus Genz <linuslinuxgenz@gmail.com>
2026-01-07 23:44:45 +0100

    ✨ 0.20.6 sort && iterator header
    Signed-off-by: Linus Genz <linuslinuxgenz@gmail.com>

---
+++ include/iterator.h
@@ -0,0 +1,51 @@
+/**
+ * @file iterator.h
+ * VesperaOS - operating system for the x86_64 architecture
+ *
+ * Copyright (c) 2025 Linus Genz <mail@linusgenz.dev>
+ *
+ * Created by Linus Genz on 07.01.26.
+ *
+ * This file is part of VesperaOS.
+ *
+ * VesperaOS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * VesperaOS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with VesperaOS. If not, see <https://www.gnu.org/licenses/>.
+*/
+#ifndef VESPERAOS_ITERATOR_H
+#define VESPERAOS_ITERATOR_H
+
+template<typename Iterator>
+struct iterator_traits {
+    using difference_type = decltype(Iterator{} - Iterator{});
+    using value_type = typename Iterator::value_type;
+    using pointer = typename Iterator::pointer;
+    using reference = typename Iterator::reference;
+};
+
+template<typename T>
+struct iterator_traits<T*> {
+    using difference_type = ptrdiff_t;
+    using value_type = T;
+    using pointer = T*;
+    using reference = T&;
+};
+
+template<typename T>
+struct iterator_traits<const T*> {
+    using difference_type = ptrdiff_t;
+    using value_type = T;
+    using pointer = const T*;
+    using reference = const T&;
+};
+
+#endif //VESPERAOS_ITERATOR_H
